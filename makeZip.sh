a1b8d984c11c0795b7ce9bba95b9544b20bdd312
IIMURA Takuji <uirou.j@gmail.com>
2020-03-07 18:39:54 +0900

    Icon以外の画像ファイルを.zipに含めないように

--- makeZip.sh
+++ makeZip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f CaptionSpeaker.zip
-zip -r CaptionSpeaker.zip CaptionSpeaker/*.js CaptionSpeaker/*.json CaptionSpeaker/_locales/*/messages.json CaptionSpeaker/icon/*.png CaptionSpeaker/*.html
+zip -r CaptionSpeaker.zip CaptionSpeaker/*.js CaptionSpeaker/*.json CaptionSpeaker/_locales/*/messages.json CaptionSpeaker/icon/Icon*.png CaptionSpeaker/*.html
